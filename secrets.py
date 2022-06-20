e0e431e26fd16ecae04e085fda38255c54987054
RomirVarshney <romir.varshney@gmail.com>
2022-06-19 23:01:28 -0400

    Added Files
    Added the files to the (again) created repository.

---
+++ secrets.py
@@ -0,0 +1,19 @@
+#Client ID 519d971201994261b73be900b361d71b
+#Client Secret 81a8f4609e7a46369b91c35779ebe986
+
+#https://accounts.spotify.com/authorize?client_id=519d971201994261b73be900b361d71b&response_type=code&redirect_uri=https%3A%2F%2Fgithub.com%2FRomirVarshney
+
+#curl -H "Authorization: Basic NTE5ZDk3MTIwMTk5NDI2MWI3M2JlOTAwYjM2MWQ3MWI6ODFhOGY0NjA5ZTdhNDYzNjliOTFjMzU3NzllYmU5ODY=" -d grant_type=authorization_code -d code=AQALpCrcLXM2NTK-DQXxfXdUircqdfxEcuTC_mlMITGwHZxKVOs_xktN4aNiiXRyk_CPRe-Bh_A3sIDVNNKipToU_fJxlztrETGHV3DyPjjLudBhfu3ScBihgBlBMjRwz9gF-Hg4TZ84I-4JKB-KpDnLriqJ6bWN8V86IOkc-RIbURKs -d redirect_uri=https%3A%2F%2Fgithub.com%2FRomirVarshney "https://accounts.spotify.com/api/token"
+
+
+spotify_token = "BQD3gUuwKA4cuNR_AhN2Ktv9gjbuJNqrHqXNhHqDHMyiVrOpd2FpUTaxRPldX7MDWZBmNgEOJ_aBTlNEY7ey6-jGblmC3PIwr5AWY6zuvO_vcOjAYmH4AsIT2PVSQInZfTwk6-YW4A0Hphzm0bRDo5_fML-5h-kN8Vjvk-O18A"
+spotify_user_id = "312nkwcfxygzttuwecwadp77lhxe"
+
+#export SPOTIPY_CLIENT_ID = ""
+#export SPOTIPY_CLIENT_SECRET = "81a8f4609e7a46369b91c35779ebe986"
+#export SPOTIPY_REDIRECT_URI="https://github.com/RomirVarshney"
+
+ID = "519d971201994261b73be900b361d71b"
+SECRET = "81a8f4609e7a46369b91c35779ebe986"
+URI="https://github.com/RomirVarshney"
+scope = 'user-top-read user-library-read playlist-read-private'
